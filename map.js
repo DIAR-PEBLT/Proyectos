bd708edf0435977fc674b9086ca8f165c0a25a48
Hugo Añamuro <hugo.aluque@gmail.com>
2022-02-25 12:39:17 -0500

    update markers

--- map.js
+++ map.js
@@ -18,7 +18,7 @@
 // Configuracion del Marcador
 var marcador_rojo = L.icon({
     iconUrl: "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png",
-    iconSize: [30, 50], // size of the icon
+    iconSize: [20, 33], // size of the icon
     //shadowSize: [50, 64], // size of the shadow
     //iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
     //shadowAnchor: [4, 62], // the same for the shadow
@@ -27,7 +27,7 @@ var marcador_rojo = L.icon({
 
 var marcador_Amarillo = L.icon({
     iconUrl: "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-yellow.png",
-    iconSize: [30, 50], // size of the icon
+    iconSize: [20, 33], // size of the icon
     //shadowSize: [50, 64], // size of the shadow
     //iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
     //shadowAnchor: [4, 62], // the same for the shadow
@@ -36,7 +36,7 @@ var marcador_Amarillo = L.icon({
 
 var marcador_Verde = L.icon({
     iconUrl: "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png",
-    iconSize: [30, 50], // size of the icon
+    iconSize: [22, 33], // size of the icon
     //shadowSize: [50, 64], // size of the shadow
     //iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
     //shadowAnchor: [4, 62], // the same for the shadow
@@ -135,13 +135,87 @@ var Urinsaya = L.marker([-14.269265, -69.986970], {
         title: "MEJORAMIENTO Y AMPLIACION DEL SERVICIO DE AGUA PARA EL SISTEMA DE RIEGO CONAVIRE, CAHUALLA Y CARI CARI",
     }).bindPopup("<h2> <center>MEJORAMIENTO Y AMPLIACION DEL SERVICIO DE AGUA PARA EL SISTEMA DE RIEGO CONAVIRE, CAHUALLA Y CARI CARI DEL DISTRITO DE MAÑAZO - PROVINCIA DE PUNO - DEPARTAMENTO DE PUNO</center></h2><table class='tg'> <thead> <tr> <th class='tg-n533'>Código de Idea</th> <th class='tg-n533'>Presupuesto</th> <th class='tg-n533'>Avance Fisico</th> <th class='tg-n533'>Estado</th> <th class='tg-n533'>Plazo de Ejecución (Días)</th> <th class='tg-n533'>N. Beneficiarios</th> <th class='tg-f0bj'>Ubicación</th> <th class='tg-n533'>Mas Detalle</th> </tr> </thead> <tbody> <tr> <td class='tg-wd1d'>29547</td> <td class='tg-wd1d'>29,000,000.00</td> <td class='tg-wd1d'>ND</td> <td class='tg-wd1d'>ND</td> <td class='tg-wd1d'>ND</td> <td class='tg-wd1d'>ND</td> <td class='tg-wd1d'>Mañazo - Puno - Puno</td> <td class='tg-wd1d'>ND</td> </tr> </tbody> </table> ", {
         maxWidth: "auto"
-    });
+    }),
+    pisquichiri_camja = L.marker([-16.449083, -69.285856], {
+        icon: marcador_rojo,
+        draggable: false,
+        title: "CONSTRUCCION DE SISTEMA DE RIEGO DE LAS LOCALIDADES DE PISQUICHIRI Y CAJNAJO",
+    }).bindPopup("<h2> <center>CONSTRUCCION DE SISTEMA DE RIEGO DE LAS LOCALIDADES DE PISQUICHIRI Y CAJNAJO, DE LA COMUNIDAD DE PATACOLLO, DISTRITO DE ZEPITA, PROVINCIA DE CHUCUITO-PUNO </center></h2><table class='tg'> <thead> <tr> <th class='tg-n533'>SNIP</th> <th class='tg-n533'>Presupuesto</th> <th class='tg-n533'>Avance Fisico</th> <th class='tg-n533'>Estado</th> <th class='tg-n533'>Plazo de Ejecución (Días)</th> <th class='tg-n533'>N. Beneficiarios</th> <th class='tg-f0bj'>Ubicación</th> <th class='tg-n533'>Mas Detalle</th> </tr> </thead> <tbody> <tr> <td class='tg-wd1d'>2146317</td> <td class='tg-wd1d'>150,771.00</td> <td class='tg-wd1d'>100%</td> <td class='tg-wd1d'>ND</td> <td class='tg-wd1d'>90</td> <td class='tg-wd1d'>725 productores</td> <td class='tg-wd1d'>Zepita - Chucuito - Puno</td> <td class='tg-wd1d'><a href='https://ofi5.mef.gob.pe/invierte/informeCierre/consultaCierre/2146317'target='blank'>Click Aqui<a><br></td> </tr> </tbody> </table> ", {
+        maxWidth: "auto"
+    }),
+    pasanacollo = L.marker([-14.532436, -70.524779], {
+        icon: marcador_rojo,
+        draggable: false,
+        title: "MEJORAMIENTO Y AMPLIACIÓN DEL SERVICIO DE AGUA PARA EL SISTEMA DE RIEGO PASANACOLLO",
+    }).bindPopup("<h2> <center>MEJORAMIENTO Y AMPLIACIÓN DEL SERVICIO DE AGUA PARA EL SISTEMA DE RIEGO PASANACOLLO, EN LA COMUNIDAD DE PASANACOLLO, DISTRITO DE NUÑOA, PROVINCIA DE MELGAR, REGIÓN PUNO</center></h2><table class='tg'> <thead> <tr> <th class='tg-n533'>SNIP</th> <th class='tg-n533'>Presupuesto</th> <th class='tg-n533'>Avance Fisico</th> <th class='tg-n533'>Estado</th> <th class='tg-n533'>Plazo de Ejecución (Días)</th> <th class='tg-n533'>N. Beneficiarios</th> <th class='tg-f0bj'>Ubicación</th> <th class='tg-n533'>Mas Detalle</th> </tr> </thead> <tbody> <tr> <td class='tg-wd1d'>2232348</td> <td class='tg-wd1d'>10,453,134.91</td> <td class='tg-wd1d'>98%</td> <td class='tg-wd1d'>No Culminada</td> <td class='tg-wd1d'>419</td> <td class='tg-wd1d'>850 productores</td> <td class='tg-wd1d'>Nuñoa- Melgar - Puno</td> <td class='tg-wd1d'><a href='https://ofi5.mef.gob.pe/invierte/informeCierre/consultaCierre/2232348'target='blank'>Click Aqui</a><br></td> </tr> </tbody> </table> ", {
+        maxWidth: "auto"
+    }),
+    molino_kapia = L.marker([-16.396395, -69.119744], {
+        icon: marcador_rojo,
+        draggable: false,
+        title: "MEJORAMIENTO Y AMPLIACION DE SISTEMA DE RIEGO EN LA LOCALIDAD DE MOLINO KAPIA",
+    }).bindPopup("<h2> <center>MEJORAMIENTO Y AMPLIACION DE SISTEMA DE RIEGO EN LA LOCALIDAD DE MOLINO KAPIA DISTRITO DE ZEPITA, PROVINCIA DE CHUCUITO - PUNO</center></h2> <table class='tg'> <thead> <tr> <th class='tg-n533'>SNIP</th> <th class='tg-n533'>Presupuesto</th> <th class='tg-n533'>Avance Fisico</th> <th class='tg-n533'>Estado</th> <th class='tg-n533'>Plazo de Ejecución (Días)</th> <th class='tg-n533'>N. Beneficiarios</th> <th class='tg-f0bj'>Ubicación</th> <th class='tg-n533'>Mas Detalle</th> </tr> </thead> <tbody> <tr> <td class='tg-wd1d'>2146314</td> <td class='tg-wd1d'>373,528.75</td> <td class='tg-wd1d'>100%</td> <td class='tg-wd1d'>Culminado / Pendiente a Transferir</td> <td class='tg-wd1d'>90</td> <td class='tg-wd1d'>600 productores</td> <td class='tg-wd1d'>Zepita - Nuñoa - Puno</td> <td class='tg-wd1d'><a href='https://ofi5.mef.gob.pe/invierte/formato/verFichaSNIP/136989/4/0'target='blank'>Click Aqui</a><br></td></tr></tbody> </table>", {
+        maxWidth: "auto"
+    }),
+    canteria = L.marker([-15.559063, -70.269675], {
+        icon: marcador_rojo,
+        draggable: false,
+        title: "REHABILITACIÓN DE LA IRRIGACIÓN CANTERIA",
+    }).bindPopup("<h2><center>REHABILITACIÓN DE LA IRRIGACIÓN CANTERIA</center></h2><table class='tg'> <thead> <tr> <th class='tg-n533'>SNIP</th> <th class='tg-n533'>Presupuesto</th> <th class='tg-n533'>Avance Fisico</th> <th class='tg-n533'>Estado</th> <th class='tg-n533'>Plazo de Ejecución (Días)</th> <th class='tg-n533'>N. Beneficiarios</th> <th class='tg-f0bj'>Ubicación</th> <th class='tg-n533'>Mas Detalle</th> </tr> </thead> <tbody> <tr> <td class='tg-wd1d'>2094964</td> <td class='tg-wd1d'>12,459,367.48</td> <td class='tg-wd1d'>96 %</td> <td class='tg-wd1d'>No culminada</td> <td class='tg-wd1d'>180</td> <td class='tg-wd1d'>ND</td> <td class='tg-wd1d'>Lampa - Lampa - Puno</td> <td class='tg-wd1d'><a href='https://ofi5.mef.gob.pe/invierte/informeCierre/consultaCierre/2094964'target='blank'>Click Aqui</a><br></td> </tr> </tbody> </table>", {
+        maxWidth: "auto"
+    }),
+    molienda = L.marker([-14.284025, -69.974473], {
+        icon: marcador_rojo,
+        draggable: false,
+        title: "MEJORAMIENTO DEL SISTEMA RIEGO MOLIENDA",
+    }).bindPopup("<h2><center>MEJORAMIENTO DEL SISTEMA RIEGO MOLIENDA, DISTRITO DE CRUCERO - CARABAYA - PUNO</center></h2><table class='tg'> <thead> <tr> <th class='tg-n533'>SNIP</th> <th class='tg-n533'>Presupuesto</th> <th class='tg-n533'>Avance Fisico</th> <th class='tg-n533'>Estado</th> <th class='tg-n533'>Plazo de Ejecución (Días)</th> <th class='tg-n533'>N. Beneficiarios</th> <th class='tg-f0bj'>Ubicación</th> <th class='tg-n533'>Mas Detalle</th> </tr> </thead> <tbody> <tr> <td class='tg-wd1d'>2149946</td> <td class='tg-wd1d'>7,543,081.00</td> <td class='tg-wd1d'>ND</td> <td class='tg-wd1d'>liquidacion obserbada</td> <td class='tg-wd1d'>360</td> <td class='tg-wd1d'>787 productores (120 familias)</td> <td class='tg-wd1d'>Crucero - Carabaya - Puno</td> <td class='tg-wd1d'><a href='https://ofi5.mef.gob.pe/invierte/ejecucion/verFichaEjecucion/2149946'target='blank'>Click Aqui</a><br></td> </tr> </tbody> </table>", {
+        maxWidth: "auto"
+    }),
+    Santa_licia_ca_lampa = L.marker([-15.667675, -70.652650], {
+        icon: marcador_Amarillo,
+        draggable: false,
+        title: "MEJORAMIENTO Y AMPLIACION DEL SERVICIO DE AGUA PARA EL SISTEMA DE RIEGO SANTA LUCIA-CABANILLA-LAMPA",
+    }).bindPopup("<h2> <center>MEJORAMIENTO Y AMPLIACION DEL SERVICIO DE AGUA PARA EL SISTEMA DE RIEGO SANTA LUCIA-CABANILLA-LAMPA, DISTRITOS DE SANTA LUCIA, CABANILLA Y LAMPA, PROVINCIA DE LAMPA, REGIÓN PUNO</center></h2><table class='tg'> <thead> <tr> <th class='tg-n533'>SNIP</th> <th class='tg-n533'>Presupuesto</th> <th class='tg-n533'>Avance Fisico</th> <th class='tg-n533'>Estado</th> <th class='tg-n533'>Plazo de Ejecución (Días)</th> <th class='tg-n533'>N. Beneficiarios</th> <th class='tg-f0bj'>Ubicación</th> <th class='tg-n533'>Mas Detalle</th> </tr> </thead> <tbody> <tr> <td class='tg-wd1d'>2277099</td> <td class='tg-wd1d'>240,531,550.00</td> <td class='tg-wd1d'>ND</td> <td class='tg-wd1d'>Registro de Expediente Técnico</td> <td class='tg-wd1d'>ND</td> <td class='tg-wd1d'>ND</td> <td class='tg-wd1d'>Santa Lucia/Cabanilla/Lampa - Lampa - Puno</td> <td class='tg-wd1d'><a href='https://ofi5.mef.gob.pe/invierte/ejecucion/verFichaEjecucion/2277099'target='blank'>Click Aqui</a><br></td> </tr> </tbody> </table>", {
+        maxWidth: "auto"
+    }),
+    ampliacion_cabana = L.marker([-15.706292, -70.358843], {
+        icon: marcador_Amarillo,
+        draggable: false,
+        title: "AMPLIACION DEL SERVICIO DE AGUA PARA EL SISTEMA DE RIEGO CABANA",
+    }).bindPopup("<h2> <center>AMPLIACION DEL SERVICIO DE AGUA PARA EL SISTEMA DE RIEGO CABANA, DISTRITO DE CABANA - PROVINCIA DE SAN ROMAN - DEPARTAMENTO DE PUNO</center></h2><table class='tg'> <thead> <tr> <th class='tg-n533'>SNIP</th> <th class='tg-n533'>Presupuesto</th> <th class='tg-n533'>Avance Fisico</th> <th class='tg-n533'>Estado</th> <th class='tg-n533'>Plazo de Ejecución (Días)</th> <th class='tg-n533'>N. Beneficiarios</th> <th class='tg-f0bj'>Ubicación</th> <th class='tg-n533'>Mas Detalle</th> </tr> </thead> <tbody> <tr> <td class='tg-wd1d'>2339393</td> <td class='tg-wd1d'>51,868,469.07</td> <td class='tg-wd1d'>ND</td> <td class='tg-wd1d'>Registro de Expediente Técnico</td> <td class='tg-wd1d'>ND</td> <td class='tg-wd1d'>ND</td> <td class='tg-wd1d'>Cabana - San Román - Puno</td> <td class='tg-wd1d'><a href='https://ofi5.mef.gob.pe/invierte/ejecucion/verFichaEjecucion/2339393'target='blank'>Click Aqui</a><br></td> </tr> </tbody> </table>", {
+        maxWidth: "auto"
+    }),
+    ampliacion_vilque_mañazo = L.marker([-15.784944, -70.307327], {
+        icon: marcador_Amarillo,
+        draggable: false,
+        title: "AMPLIACION DEL SERVICIO DE AGUA PARA EL SISTEMA DE RIEGO VILQUE - MAÑAZO",
+    }).bindPopup("<h2> <center>AMPLIACION DEL SERVICIO DE AGUA PARA EL SISTEMA DE RIEGO VILQUE - MAÑAZO (SECTORES COTAÑA, MOLINO, PHESCACHA, PETRARIA,C.C. VISCACHANI, QUERI, SAN JUAN DE QUEARAYA Y C.C. CANLLACOLLO) EN LOS DISTRITOS DE VILQUE Y MAÑAZO DE LA  PROVINCIA DE PUNO - DEPARTAMENTO DE PUNO</center></h2><table class='tg'> <thead> <tr> <th class='tg-n533'>SNIP</th> <th class='tg-n533'>Presupuesto</th> <th class='tg-n533'>Avance Fisico</th> <th class='tg-n533'>Estado</th> <th class='tg-n533'>Plazo de Ejecución (Días)</th> <th class='tg-n533'>N. Beneficiarios</th> <th class='tg-f0bj'>Ubicación</th> <th class='tg-n533'>Mas Detalle</th> </tr> </thead> <tbody> <tr> <td class='tg-wd1d'>2471016</td> <td class='tg-wd1d'>50,023,551.84</td> <td class='tg-wd1d'>ND</td> <td class='tg-wd1d'>Expediente Técnico</td> <td class='tg-wd1d'>ND</td> <td class='tg-wd1d'>ND</td> <td class='tg-wd1d'>Vilque/Mañazo - San Román - Puno</td> <td class='tg-wd1d'><a href='https://ofi5.mef.gob.pe/invierte/seguimiento/verFichaSeguimiento/2471016'target='blank'>Click Aqui</a><br></td> </tr> </tbody> </table>", {
+        maxWidth: "auto"
+    }),
+    Canal_n = L.marker([-14.882257, -70.822267], {
+        icon: marcador_rojo,
+        draggable: false,
+        title: "MEJORAMIENTO Y AMPLIACION DEL SERVICIO DE AGUA PARA EL SISTEMA DE RIEGO CANAL N",
+    }).bindPopup("<h2> <center>MEJORAMIENTO Y AMPLIACION DEL SERVICIO DE AGUA PARA EL SISTEMA DE RIEGO CANAL N, EN LOS SECTORES DE CORANI, AQUESAYA, INCALARKA, CHALLAPATA Y MELGAR, DE LOS DISTRITOS DE CUPI Y UMACHIRI, PROVINCIA DE MELGAR, REGION PUNO</center></h2><table class='tg'> <thead> <tr> <th class='tg-n533'>SNIP</th> <th class='tg-n533'>Presupuesto</th> <th class='tg-n533'>Avance Fisico</th> <th class='tg-n533'>Estado</th> <th class='tg-n533'>Plazo de Ejecución (Días)</th> <th class='tg-n533'>N. Beneficiarios</th> <th class='tg-f0bj'>Ubicación</th> <th class='tg-n533'>Mas Detalle</th> </tr> </thead> <tbody> <tr> <td class='tg-wd1d'>2173526</td> <td class='tg-wd1d'>35,196,835.5</td> <td class='tg-wd1d'>ND</td> <td class='tg-wd1d'>Culminado/Pendiente Liquidación</td> <td class='tg-wd1d'>ND</td> <td class='tg-wd1d'>ND</td> <td class='tg-wd1d'>Cupi/Umachiri - Melgar - Puno</td> <td class='tg-wd1d'><a href='https://ofi5.mef.gob.pe/invierte/formato/verFichaSNIP/222886/0/0'target='blank'>Click Aqui</a><br></td> </tr> </tbody> </table>", {
+        maxWidth: "auto"
+    })
+
+
+;
+
+
+
+
 
 //Agrupando los Marcadores de Tipo Proyecto
-var Proyectos = L.layerGroup([Urinsaya, huancarani, sucre, llallimayo, jilas]);
+var Proyectos = L.layerGroup([Urinsaya, huancarani, sucre, llallimayo, jilas,
+    pisquichiri_camja, pasanacollo, molino_kapia, canteria, molienda, Canal_n
+]);
 
 //Agrupando los Marcadores de Tipo Estudio
-var Estudio = L.layerGroup([Viluyo_lara, paylla_mira, Kcatawi, sur_paylla]);
+var Estudio = L.layerGroup([Viluyo_lara, paylla_mira, Kcatawi, sur_paylla,
+    Santa_licia_ca_lampa, ampliacion_cabana, ampliacion_vilque_mañazo
+]);
 
 // Agrpando los Marcadores de Tipo Idea de Proyecto
 
